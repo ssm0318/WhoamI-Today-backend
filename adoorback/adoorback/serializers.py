eb2e3097d5b5bf96eb3f827999d9f7695caf6cf8
Yuri Kim <njs03332@gmail.com>
2023-07-22 13:57:33 +0900

    feat: #42 change current_user_liked to current_user_like_id

--- adoorback/adoorback/serializers.py
+++ adoorback/adoorback/serializers.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from adoorback.utils.content_types import get_generic_relation_type
+from like.models import Like
+
 User = get_user_model()
 
 
 class AdoorBaseSerializer(serializers.ModelSerializer):
     like_count = serializers.SerializerMethodField(read_only=True)
-    current_user_liked = serializers.SerializerMethodField(read_only=True)
+    current_user_like_id = serializers.SerializerMethodField(read_only=True)
 
     def validate(self, attrs):
         if len(attrs.get('content')) == 0:
@@ -19,11 +22,13 @@ class AdoorBaseSerializer(serializers.ModelSerializer):
             return None
         return obj.liked_user_ids.count()
 
-    def get_current_user_liked(self, obj):
+    def get_current_user_like_id(self, obj):
         current_user_id = self.context['request'].user.id
-        return current_user_id in obj.liked_user_ids
+        content_type_id = get_generic_relation_type(obj.type).id
+        like = Like.objects.filter(user_id=current_user_id, content_type_id=content_type_id, object_id=obj.id)
+        return like[0].id if like else None
 
     class Meta:
         model = None
-        fields = ['id', 'type', 'content', 'like_count', 'current_user_liked', 'created_at']
+        fields = ['id', 'type', 'content', 'like_count', 'current_user_like_id', 'created_at']
         validators = []
