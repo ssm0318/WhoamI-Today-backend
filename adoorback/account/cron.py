69d01537d2042bc38102a11ef4770b9862f4b970
h-yes-oo <hyesoo5115@naver.com>
2023-09-09 16:10:45 +0900

    fix: #26 send daily noti with consideration of user timezone

--- adoorback/account/cron.py
+++ adoorback/account/cron.py
@@ -1,6 +1,7 @@
-from datetime import timedelta, time, datetime
+from datetime import time
 
-from django.utils.timezone import make_aware
+from django.utils import timezone
+from backports.zoneinfo import ZoneInfo
 from django.contrib.auth import get_user_model
 from django_cron import CronJobBase, Schedule
 
@@ -11,7 +12,7 @@ User = get_user_model()
 
 
 class SendDailyWhoAmINotiCronJob(CronJobBase):
-    RUN_EVERY_MINS = 1  # every minute
+    RUN_EVERY_MINS = 60  # every 1 hour
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'account.send_daily_who_am_i_noti_cron_job'
@@ -19,13 +20,10 @@ class SendDailyWhoAmINotiCronJob(CronJobBase):
     def do(self):
         print('=========================')
         print("Creating daily notifications for WhoAmI...............")
-        current_time = datetime.now().time()
-        users = User.objects.filter(noti_time=time(current_time.hour, current_time.minute))
-        if current_time == time(16, 0):
-            users |= User.objects.filter(noti_time=None)  # add users who did not set noti_time
+
         admin = User.objects.filter(is_superuser=True).first()
         try:
-            daily_question = Question.objects.daily_questions[0].content
+            daily_question = Question.objects.daily_questions()[0].content
         except:
             print('=========================')
             print('daily question does not exist!')
@@ -34,15 +32,22 @@ class SendDailyWhoAmINotiCronJob(CronJobBase):
             return
 
         num_notis_before = Notification.objects.admin_only().count()
+        all_users = User.objects.all()
 
-        for user in users:
-            Notification.objects.create(user=user,
-                                        actor=admin,
-                                        target=admin,
-                                        origin=admin,
-                                        message_ko=f"{user.username}님, 오늘의 후엠아이를 남겨보세요! - {daily_question}",
-                                        message_en=f"{user.username}, time to leave your whoami for today! - {daily_question}",
-                                        redirect_url='/home')
+        for user in all_users:
+            if not user.noti_on:
+                continue
+            if user.noti_time is None:
+                continue
+            if user.noti_time == time(timezone.now().astimezone(ZoneInfo(user.timezone)).hour, 0):
+                Notification.objects.create(user=user,
+                                            actor=admin,
+                                            target=admin,
+                                            origin=admin,
+                                            message_ko=f"{user.username}님, 오늘의 후엠아이를 남겨보세요! - {daily_question}",
+                                            message_en=f"{user.username}, time to leave your whoami for today! - {daily_question}",
+                                            redirect_url='/home')
+
         num_notis_after = Notification.objects.admin_only().count()
         print(f'{num_notis_after - num_notis_before} notifications sent!')
         print('=========================')
