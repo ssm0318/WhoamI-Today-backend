c7d0224714ec38e2a2c44ff5fafa26e446ae5eb6
h-yes-oo <hyesoo5115@naver.com>
2023-09-09 21:12:58 +0900

    feat: #91 change daily noti cron job to run at exact times

--- adoorback/account/cron.py
+++ adoorback/account/cron.py
@@ -12,9 +12,13 @@ User = get_user_model()
 
 
 class SendDailyWhoAmINotiCronJob(CronJobBase):
-    RUN_EVERY_MINS = 60  # every 1 hour
+    # run every hour at 0 minute
+    RUN_AT_TIMES = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
+                    '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
+                    '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
+                    '19:00', '20:00', '21:00', '22:00', '23:00']
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'account.send_daily_who_am_i_noti_cron_job'
 
     def do(self):
