6339b477a9898396ad75bfa8f52b095495f458be
h-yes-oo <hyesoo5115@naver.com>
2023-10-14 15:36:33 +0900

    fix: #72 get user in case-sensitive manner when logging in

--- adoorback/account/backends.py
+++ adoorback/account/backends.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.backends import ModelBackend, UserModel
 from django.db.models import Q
 
+
 class CustomModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            user = UserModel.objects.get(Q(username__exact=username) | Q(email__exact=username))
         except UserModel.DoesNotExist:
             UserModel().set_password(password)
         else:
