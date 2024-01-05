722b30bac0b73b572c2771538a2363dd9b280919
h-yes-oo <hyesoo5115@naver.com>
2024-01-05 23:46:49 +0900

    feat: implement GET /api/chat/rooms/

--- adoorback/chat/urls.py
+++ adoorback/chat/urls.py
@@ -4,12 +4,8 @@ from django.urls import path
 from . import views
 
 
-<<<<<<< HEAD
 urlpatterns = [
     path('rooms/', views.ChatRoomList.as_view()),
     path('rooms/<int:pk>/', views.ChatRoomFriendList.as_view()),
     path('<int:pk>/messages/', views.ChatMessagesListView.as_view()),
 ]
-=======
-urlpatterns = []
->>>>>>> 942c1c5 (feat: basic settings for chat)
