40ae85a562df7f13a3b205061853575c9ff2c104
ssm0318 <slove0318@snu.ac.kr>
2024-02-10 00:31:56 -0800

    feat: #134 add view, serializer

--- adoorback/note/views.py
+++ adoorback/note/views.py
@@ -1,3 +1,20 @@
+from django.db import transaction
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from adoorback.utils.validators import adoor_exception_handler
+from note.models import Note
+
+
+class NoteList(generics.CreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_exception_handler(self):
+        return adoor_exception_handler
+
+    @transaction.atomic
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
