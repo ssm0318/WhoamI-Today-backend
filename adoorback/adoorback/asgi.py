6aeb2146faa4dfdbea01b8f7f3c8f84e34c4e28e
Yuri Kim <njs03332@gmail.com>
2024-03-02 08:15:22 +0900

    feat: #112 add minor format change

--- adoorback/adoorback/asgi.py
+++ adoorback/adoorback/asgi.py
@@ -7,19 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
 
-from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
-
-import chat.routing
 
 application = ProtocolTypeRouter(
     {
