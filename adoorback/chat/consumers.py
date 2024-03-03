90eff193dd6d4d554521559ed035f1672a371914
Yuri Kim <njs03332@gmail.com>
2024-03-03 21:08:21 +0900

    feat: #117 implement chat list web socket

--- adoorback/chat/consumers.py
+++ adoorback/chat/consumers.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-from datetime import datetime
+from django.db.models import Q
 
+from chat.models import Message, ChatRoom, UserChatActivity
+from utils.helpers import update_last_read_message
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
@@ -17,6 +20,11 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
+        # Update last read message for the user
+        user = self.scope["user"]
+        chat_room = ChatRoom.objects.get(id=self.room_id)
+        update_last_read_message(user, chat_room)
+
     def disconnect(self, close_code):
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
@@ -26,26 +34,81 @@ class ChatConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        content = text_data_json["content"]
         user_id = text_data_json["userId"]
         user_name = text_data_json["userName"]
         timestamp = datetime.utcnow()
         timestamp_str = timestamp.strftime("%Y-%m-%dT%H:%M:%S.000+00:00")
 
+        # Save message to database
+        new_message = Message.objects.create(sender_id=user_id, content=content, chat_room_id=self.room_id, timestamp=timestamp)
+        message_id = new_message.id
+
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_id, {"type": "chat.message", "message": message, "userName": user_name, "timestamp": timestamp_str}
+            self.room_group_id, {
+                "type": "chat.message", 
+                "content": content, 
+                "messageId": message_id,
+                "userName": user_name, 
+                "timestamp": timestamp_str
+            }
         )
 
-        # Save message to database
-        from chat.models import Message
-        Message.objects.create(sender_id=user_id, content=message, chat_room_id=self.room_id, timestamp=timestamp)
+        # Send message to chatroom_list group
+        chat_room = ChatRoom.objects.get(id=self.room_id)
+        recipients = chat_room.users.exclude(id=user_id)
+        for r in recipients:
+            async_to_sync(self.channel_layer.group_send)(
+                f"user_{r.id}_chat_list", {
+                    "type": "chatroom.message",
+                    "roomId": self.room_id,
+                    "content": content,
+                    "timestamp": timestamp_str,
+                    "unreadCnt": chat_room.unread_cnt(r)
+                }
+            )
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event["message"]
+        content = event["content"]
         user_name = event["userName"]
         timestamp = event["timestamp"]
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message, "userName": user_name, "timestamp": timestamp}))
+        self.send(text_data=json.dumps({"content": content, "userName": user_name, "timestamp": timestamp}))
+
+        # Save read status to database
+        user = self.scope["user"]
+        chat_room = ChatRoom.objects.get(id=self.room_id)
+        update_last_read_message(user, chat_room)
+
+
+class ChatRoomListConsumer(WebsocketConsumer):
+    def connect(self):
+        # Make group for each user
+        self.user_id = self.scope["user"]
+        self.user_group_id = f"user_{self.user_id}_chat_list"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.user_group_id, self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.user_group_id, self.channel_name
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        content = event["content"]
+        room_id = event["roomId"]
+        timestamp = event["timestamp"]
+        unread_cnt = event["unreadCnt"]
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            "content": content, "roomId": room_id, "timestamp": timestamp, "unreadCnt": unread_cnt
+        }))
--- adoorback/chat/serializers.py
+++ adoorback/chat/serializers.py
@@ -5,6 +5,7 @@ from account.serializers import UserMinimalSerializer
 
 class ChatRoomSerializer(serializers.ModelSerializer):
     participants = serializers.SerializerMethodField(read_only=True)
+    unread_cnt = serializers.SerializerMethodField(read_only=True)
 
     def get_participants(self, obj):
         request = self.context.get('request')
@@ -13,10 +14,17 @@ class ChatRoomSerializer(serializers.ModelSerializer):
             participants = obj.users.exclude(id=current_user.id)
             return UserMinimalSerializer(participants, many=True).data
         return []
+    
+    def get_unread_cnt(self, obj):
+        request = self.context.get('request')
+        if request and request.user:
+            current_user = request.user
+            return obj.unread_cnt(current_user)
+        return 0
 
     class Meta:
         model = ChatRoom
-        fields = ['id', 'participants', 'last_message_content', 'last_message_time', 'active']
+        fields = ['id', 'participants', 'last_message_content', 'last_message_time', 'active', 'unread_cnt']
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserMinimalSerializer(read_only=True)
