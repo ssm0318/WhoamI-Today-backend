9d4023f6e3a6bb38e5fdd345e184211050e6e886
Yuri Kim <njs03332@gmail.com>
2024-03-03 21:59:50 +0900

    feat: #117 add routing for ChatRoomListConsumer

--- adoorback/chat/routing.py
+++ adoorback/chat/routing.py
@@ -5,4 +5,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat_list/$", consumers.ChatRoomListConsumer.as_asgi()),
 ]
