a347f55545486edf1a3c8877b9bf8abe89e3a6d6
ssm0318 <slove0318@snu.ac.kr>
2024-04-26 16:45:48 -0700

    remove unnecessary code

--- adoorback/adoorback/settings/base.py
+++ adoorback/adoorback/settings/base.py
@@ -208,9 +208,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'adoorback', 'static'),
-)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
