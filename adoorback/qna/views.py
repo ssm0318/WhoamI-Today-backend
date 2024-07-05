82d1d16e40557d295940bb81a67e43e774c7e0aa
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2024-07-05 15:28:38 -0400

    (#259) Comment pagination (#268)
    * paginate note comments
    
    * paginate response comments
    
    * (#264) fix uwsgi settings (#265)
    
    * feat: #264 fix uwsgi settings
    
    * feat: #264 remove unnecessary uwsgi settings
    
    * Update README.md
    
    * (#261) add bio and track id (#269)
    
    * add bio and track id
    
    * (#264) fix uwsgi settings (#265)
    
    * feat: #264 fix uwsgi settings
    
    * feat: #264 remove unnecessary uwsgi settings
    
    * Update README.md
    
    * return null when no check in
    
    * add bio and track id
    
    * return null when no check in
    
    ---------
    
    Co-authored-by: ssm0318 <slove0318@snu.ac.kr>
    Co-authored-by: Yuri <37107740+njs03332@users.noreply.github.com>
    Co-authored-by: Jinsun Goo <37523788+GooJinSun@users.noreply.github.com>
    
    * paginate note comments
    
    * paginate response comments
    
    * remove pagination class
    
    * remove unnecessary code
    
    ---------
    
    Co-authored-by: ssm0318 <slove0318@snu.ac.kr>
    Co-authored-by: Yuri <37107740+njs03332@users.noreply.github.com>
    Co-authored-by: Jinsun Goo <37523788+GooJinSun@users.noreply.github.com>

--- adoorback/qna/views.py
+++ adoorback/qna/views.py
@@ -98,8 +98,13 @@ class ResponseComments(generics.ListAPIView):
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(queryset, many=True)
         return DjangoResponse({'count': queryset.count(), 'results': serializer.data})
+
 
 class ResponseLikes(generics.ListAPIView):
     serializer_class = LikeSerializer
