facc0ff32c632c4d2cd1598ae8dfb53dd3db3616
Gina <koyrkr@gmail.com>
2024-08-03 00:42:22 +0900

    feat: add custom_fcm serializers

--- adoorback/custom_fcm/serializers.py
+++ adoorback/custom_fcm/serializers.py
@@ -1,7 +1,13 @@
-from rest_framework import serializers
-from .models import CustomFCMDevice
+from fcm_django.api.rest_framework import FCMDeviceSerializer
 
-class CustomFCMDeviceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomFCMDevice
-        fields = ['registration_id', 'language', 'user']
+from adoorback.adoorback import serializers
+
+class CustomFCMDeviceSerializer(FCMDeviceSerializer):
+    language = serializers.CharField(max_length=10, required=False)
+
+    class Meta(FCMDeviceSerializer.Meta):
+        fields = FCMDeviceSerializer.Meta.fields + ['language']
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        return attrs
