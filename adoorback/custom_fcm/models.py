28e93aa149e4f3a07f6a3bd4341e4fc4142d8196
Gina <koyrkr@gmail.com>
2024-08-17 17:07:29 +0900

    fix: restore not to use custom_fcm

--- adoorback/custom_fcm/models.py
+++ adoorback/custom_fcm/models.py
@@ -1,5 +1,4 @@
 from django.db import models
 from fcm_django.models import FCMDevice
 
-class CustomFCMDevice(FCMDevice):
-    language = models.CharField(max_length=10, blank=True, null=True)
+FCMDevice.add_to_class('language', models.CharField(max_length=10, blank=True, null=True))
