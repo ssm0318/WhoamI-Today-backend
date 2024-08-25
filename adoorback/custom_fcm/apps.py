c26d0bfac392a9ccec5167d1d4c7f5bcda88ed1e
Gina <koyrkr@gmail.com>
2024-08-25 21:53:05 +0900

    fix: remove unnecessary code

--- adoorback/custom_fcm/apps.py
+++ adoorback/custom_fcm/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 class CustomFcmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'custom_fcm'
-
-    def ready(self):
-        import custom_fcm.models
