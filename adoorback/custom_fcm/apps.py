51dc1832645bb10fd03f9b0c1a060c9073e06661
Gina <koyrkr@gmail.com>
2024-08-31 21:50:09 +0900

    chore: add comment

--- adoorback/custom_fcm/apps.py
+++ adoorback/custom_fcm/apps.py
@@ -4,5 +4,6 @@ import os
 class CustomFCMDeviceConfig(AppConfig):
     name = 'custom_fcm'
     default_auto_field = 'django.db.models.BigAutoField'
+    # 실배포 환경에서 제대로 된 path를 찾지 못하는 문제를 위해 필요
     path = os.path.dirname(os.path.abspath(__file__))
 
