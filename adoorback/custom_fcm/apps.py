b0eb4f9175a6f2616db838ec04daced7a529b2e0
Gina <koyrkr@gmail.com>
2024-09-07 14:56:34 +0900

    (#329) production 배포시 custom_fcm 에러 (#331)
    * fix: update default_auto_field and add path to CustomFCMDeviceConfig
    
    * chore: add comment

--- adoorback/custom_fcm/apps.py
+++ adoorback/custom_fcm/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+import os
 
 class CustomFCMDeviceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'custom_fcm'
+    default_auto_field = 'django.db.models.BigAutoField'
+    # 실배포 환경에서 제대로 된 path를 찾지 못하는 문제를 위해 필요
+    path = os.path.dirname(os.path.abspath(__file__))
+
