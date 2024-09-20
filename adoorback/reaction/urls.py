7ef5951ec152aace92ba466c42331f41a2af1f4d
Yuri Kim <njs03332@gmail.com>
2024-09-20 21:34:45 +0900

    refactor: #167 remove unused model api's

--- adoorback/reaction/urls.py
+++ adoorback/reaction/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from reaction import views
 
 urlpatterns = [
-    path('<str:ctype>/<int:oid>/', views.ReactionList.as_view(), name='response-list'),
-    path('<int:pk>/', views.ReactionDestroy.as_view(), name='reaction-destroy'),
+    # path('<str:ctype>/<int:oid>/', views.ReactionList.as_view(), name='response-list'),
+    # path('<int:pk>/', views.ReactionDestroy.as_view(), name='reaction-destroy'),
 ]
