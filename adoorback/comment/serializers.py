904b0061594fd642596e2255d52e3ceb91d2b2a4
Gina <koyrkr@gmail.com>
2024-09-21 08:13:20 +0900

    (#344) 내 댓글에 대한 좋아요 정보 노출 (#345)
    * feat: add like_user_sample in CommentBaseSerializer
    
    * feat: add get comment likes API

--- adoorback/comment/serializers.py
+++ adoorback/comment/serializers.py
@@ -24,6 +24,7 @@ class CommentBaseSerializer(AdoorBaseSerializer):
     is_reply = serializers.SerializerMethodField(read_only=True)
     target_id = serializers.SerializerMethodField()
     user_tags = serializers.SerializerMethodField()
+    like_user_sample = serializers.SerializerMethodField(read_only=True)
 
     def get_is_reply(self, obj):
         return obj.target.type == 'Comment'
@@ -34,10 +35,16 @@ class CommentBaseSerializer(AdoorBaseSerializer):
     def get_user_tags(self, obj):
         user_tags = obj.comment_user_tags
         return UserTagSerializer(user_tags, many=True, read_only=True, context=self.context).data
+    
+    def get_like_user_sample(self, obj):
+        from account.serializers import UserMinimalSerializer
+        recent_likes = obj.comment_likes.order_by('-created_at')[:3]
+        recent_users = [like.user for like in recent_likes]
+        return UserMinimalSerializer(recent_users, many=True, context=self.context).data
 
     class Meta(AdoorBaseSerializer.Meta):
         model = Comment
-        fields = AdoorBaseSerializer.Meta.fields + ['is_reply', 'is_private', 'target_id', 'user_tags']
+        fields = AdoorBaseSerializer.Meta.fields + ['is_reply', 'is_private', 'target_id', 'user_tags', 'like_user_sample']
 
 
 class CommentFriendSerializer(CommentBaseSerializer):
