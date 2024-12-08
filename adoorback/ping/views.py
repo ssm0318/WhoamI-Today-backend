8d870ec928a515ff0e0c31683edb78b5d0ef5763
Yuri Kim <njs03332@gmail.com>
2024-12-08 16:30:11 +0900

    feat: #389 mark ping as read one by one

--- adoorback/ping/views.py
+++ adoorback/ping/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
-from rest_framework import generics, exceptions
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, exceptions, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Ping, get_or_create_ping_room, mark_pings_as_read
+from .models import Ping, get_or_create_ping_room
 from .serializers import PingSerializer
 
 User = get_user_model()
@@ -18,15 +20,11 @@ class PingList(generics.ListCreateAPIView):
         try:
             connected_user = User.objects.get(id=self.kwargs.get('pk'))
             if not user.is_connected(connected_user):
-                print('??')
                 raise exceptions.PermissionDenied("You are not connected to this user")
         except User.DoesNotExist:
             raise exceptions.NotFound("Connected user not found")
 
         ping_room = get_or_create_ping_room(connected_user, user)
-
-        # mark pings in ping_room as read
-        mark_pings_as_read(user, ping_room)
 
         pings = Ping.objects.filter(ping_room=ping_room)
         return pings
@@ -45,3 +43,17 @@ class PingList(generics.ListCreateAPIView):
 
         serializer.save(sender=user, receiver=connected_user, ping_room=ping_room)
 
+
+class MarkPingAsRead(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        user = request.user
+        ping = get_object_or_404(Ping, id=pk, receiver=user)
+
+        if ping.is_read:
+            return Response({"detail": "Ping is already marked as read."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        ping.is_read = True
+        ping.save()
+        return Response({"detail": "Ping marked as read."}, status=status.HTTP_200_OK)
