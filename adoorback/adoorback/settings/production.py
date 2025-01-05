a1d3d557c32296874afa1ad87cc06636cb06dbfe
Gina Park <ginapark@aurender.com>
2025-01-05 20:35:47 +0900

    feat: implement dynamic database configuration using environment variables and enhance manage.py for better settings visibility

--- adoorback/adoorback/settings/production.py
+++ adoorback/adoorback/settings/production.py
@@ -9,16 +9,16 @@ BASE_URL = 'https://whoami.gina-park.site'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'whoamitoday'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),  
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    },
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME', 'whoamitoday'),
+#         'USER': os.environ.get('DB_USER', 'postgres'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD', ''),  
+#         'HOST': os.environ.get('DB_HOST', 'localhost'),
+#         'PORT': os.environ.get('DB_PORT', '5432'),
+#     },
+# }
 
 # sentry_sdk.init(
 #     dsn="https://3525cb8e094e49fe9973fd92ccbf456b@o486285.ingest.sentry.io/5543025",
