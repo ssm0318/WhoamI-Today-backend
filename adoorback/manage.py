f3994a8b7a59dfc14aeeb66c5942058c811af0a7
Gina Park <ginapark@aurender.com>
2025-01-05 20:35:47 +0900

    feat: implement dynamic database configuration using environment variables and enhance manage.py for better settings visibility

--- adoorback/manage.py
+++ adoorback/manage.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 def main():
     """Run administrative tasks."""
     # 환경에 따라 적절한 .env 파일 로드
+    print(os.environ.get('DJANGO_SETTINGS_MODULE'))
     if os.environ.get('DJANGO_SETTINGS_MODULE') == 'adoorback.settings.development':
         load_dotenv('.env.development')
     else:
