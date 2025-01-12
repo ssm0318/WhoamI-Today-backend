2811b6ddde914cd60521d5b0c2a34582573ed866
Yuri Kim <njs03332@gmail.com>
2025-01-12 18:37:02 +0900

    feat: #427 add additional return values to ping APIs

--- adoorback/ping/urls.py
+++ adoorback/ping/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('user/<int:pk>/', views.PingList.as_view(), name='ping_list'),
-    path('<int:pk>/mark-as-read/', views.MarkPingAsRead.as_view(), name='mark-ping-as-read'),
 ]
