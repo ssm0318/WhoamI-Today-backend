d6e372a22d31d292f93573bca8d07e53208c9c8e
Yuri Kim <njs03332@gmail.com>
2025-01-25 19:56:20 +0900

    feat: #433, #436 add version related fields to user

--- adoorback/account/models.py
+++ adoorback/account/models.py
@@ -45,6 +45,23 @@ ETHNICITY_CHOICES = (
     (5, _('백인 (White)')),
 )
 
+VERSION_CHOICES = (
+    ('default', 'Default'),
+    ('experiment', 'Experiment'),
+)
+
+USER_GROUP_CHOICES = (
+    ('group_1', 'Group 1: only ver.D'),
+    ('group_2', 'Group 2: only ver.E'),
+    ('group_3', 'Group 3: ver.D -> ver.E'),
+    ('group_4', 'Group 4: ver.E -> ver.D'),
+)
+
+USER_TYPE_CHOICES = (
+    ('direct', 'Direct Participant'),
+    ('indirect', 'Indirect Participant'),
+)
+
 
 class OverwriteStorage(FileSystemStorage):
     base_url = urllib.parse.urljoin(settings.BASE_URL, settings.MEDIA_URL)
@@ -107,6 +124,13 @@ class User(AbstractUser, AdoorTimestampedModel, SafeDeleteModel):
     friends = models.ManyToManyField('self', symmetrical=True, blank=True)  # TODO: fade out
     favorites = models.ManyToManyField('self', symmetrical=False, related_name='favorite_of', blank=True)
     hidden = models.ManyToManyField('self', symmetrical=False, related_name='hidden_by', blank=True)
+
+    ver_changed_at = models.DateTimeField(null=True)
+    current_ver = models.CharField(max_length=20, choices=VERSION_CHOICES, default='default')
+    user_group = models.CharField(max_length=20, choices=USER_GROUP_CHOICES, default='group_1')
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='direct')
+    invited_from = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, 
+                                     related_name="invited_users")
 
     friendship_targetted_notis = GenericRelation("notification.Notification",
                                                  content_type_field='target_type',
