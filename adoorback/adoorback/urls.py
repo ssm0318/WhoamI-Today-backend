98d747d2d82204298aa13bd0220db2bf64136df8
Gina Park <koyrkr@gmail.com>
2025-02-08 07:35:45 +0900

    feat: add health check endpoint to Django URLs

--- adoorback/adoorback/urls.py
+++ adoorback/adoorback/urls.py
@@ -19,9 +19,14 @@ from django.conf.urls.static import static
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf.urls.i18n import i18n_patterns
+from django.http import JsonResponse
 
+def health_check(request):
+    return JsonResponse({"status": "ok"}, status=200)
 
 urlpatterns = i18n_patterns(
+    path("health/", health_check),
+    
     path('api/content_reports/', include('content_report.urls')),
     path('api/user_reports/', include('user_report.urls')),
     path('api/user_tags/', include('user_tag.urls')),
