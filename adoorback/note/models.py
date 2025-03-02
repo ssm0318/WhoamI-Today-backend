58fe9d2896f714e6dc634d4dfa237ba8bce99530
Gina <koyrkr@gmail.com>
2025-03-03 00:46:54 +0900

    (#502) Error occurs in the local environment at user/friends/. (#503)
    * fix: handle user1_upgrade_time in response visibility check
    
    * bugfix: #502 add branching for upgrade_time=None
    
    ---------
    
    Co-authored-by: Yuri Kim <njs03332@gmail.com>

--- adoorback/note/models.py
+++ adoorback/note/models.py
@@ -126,6 +126,8 @@ class Note(AdoorModel, SafeDeleteModel):
 
             if update_past_posts:
                 return True
+            if upgrade_time is None:  # users were close friends from the beginning
+                return True
             return self.created_at > upgrade_time
         
         return True
