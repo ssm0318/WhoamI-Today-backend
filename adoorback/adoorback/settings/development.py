eb619fbce8607d2855cf7b3193b2bbb717d3795f
Gina Park <koyrkr@gmail.com>
2025-03-15 10:47:32 +0900

    chore: update allowed hosts for development environment

--- adoorback/adoorback/settings/development.py
+++ adoorback/adoorback/settings/development.py
@@ -36,10 +36,12 @@ BASE_URL = 'http://localhost:8000'
 #     send_default_pii=True
 # )
 
-ALLOWED_HOSTS = ['ec2-3-39-220-146.ap-northeast-2.compute.amazonaws.com', 'localhost', "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_HTTPONLY = False 
+CSRF_COOKIE_SAMESITE = 'Lax'
 
 FRONTEND_URL = 'http://localhost:3000'
