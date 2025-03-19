6e526a58375cfab287b65913164f538dbbc9b121
Gina Park <koyrkr@gmail.com>
2025-03-19 10:39:40 +0900

    chore: update development database settings to use hardcoded values for easier local setup

--- adoorback/adoorback/settings/development.py
+++ adoorback/adoorback/settings/development.py
@@ -13,11 +13,10 @@ BASE_URL = 'http://localhost:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', 'whoamitoday'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': 'diivers',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
     },
 }
 
