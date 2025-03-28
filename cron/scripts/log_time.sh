9c597c06363f2799da1aa73661febb500fe2c2ee
Yuri <37107740+njs03332@users.noreply.github.com>
2025-03-28 22:38:17 +0900

    (#659) improve cron log readability (#660)
    refactor: #659 improve cron log readability

--- cron/scripts/log_time.sh
+++ cron/scripts/log_time.sh
@@ -4,4 +4,4 @@ LOG_DIR="/app/cron/log"
 LOG_FILE="${LOG_DIR}/$(date +\%Y-\%m-\%d).log"
 
 mkdir -p "$LOG_DIR"
-echo "$(date +\%Y-\%m-\%d-\%H-\%M-\%S)" >> "$LOG_FILE"
+echo -e "\n\n\n$(date +\%Y-\%m-\%d-\%H-\%M-\%S)" >> "$LOG_FILE"
