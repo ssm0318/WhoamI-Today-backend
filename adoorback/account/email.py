1ac9315894b00e546058c150785ab6cdf963a3f7
Yuri Kim <njs03332@gmail.com>
2025-04-18 22:43:56 +0900

    feat: #720 set 365 days as limit for email verification token

--- adoorback/account/email.py
+++ adoorback/account/email.py
@@ -1,19 +1,57 @@
+import datetime
 import six
 import traceback
 
 from django.conf import settings
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.core.mail import EmailMessage
-from django.utils.translation import gettext_lazy as _
-from django.utils.http import urlsafe_base64_encode
+from django.utils import timezone
+from django.utils.crypto import constant_time_compare
 from django.utils.encoding import force_bytes
+from django.utils.http import base36_to_int, urlsafe_base64_encode
+from django.utils.translation import gettext_lazy as _
 
 from adoorback.utils.alerts import send_msg_to_slack
 
 
 class ActivateTokenGenerator(PasswordResetTokenGenerator):
+    EMAIL_VERIFICATION_TIMEOUT_SECONDS = 60 * 60 * 24 * 365  # 1년
+
     def _make_hash_value(self, user, timestamp):
-        return (six.text_type(user.pk) + six.text_type(timestamp)) + six.text_type(user.is_active)
+        return (six.text_type(user.pk) + six.text_type(timestamp)) + six.text_type(user.email_verified)
+    
+    def check_token(self, user, token):
+        """
+        Check that a password reset token is correct for a given user.
+        """
+        if not (user and token):
+            return False
+        # Parse the token
+        try:
+            ts_b36, _ = token.split("-")
+        except ValueError:
+            return False
+
+        try:
+            ts = base36_to_int(ts_b36)
+        except ValueError:
+            return False
+
+        # Check that the timestamp/uid has not been tampered with
+        for secret in [self.secret, *self.secret_fallbacks]:
+            if constant_time_compare(
+                self._make_token_with_timestamp(user, ts, secret),
+                token,
+            ):
+                break
+        else:
+            return False
+
+        # Check the timestamp is within limit.
+        if (self._num_seconds(self._now()) - ts) > self.EMAIL_VERIFICATION_TIMEOUT_SECONDS:
+            return False
+
+        return True
 
 
 class EmailManager():
