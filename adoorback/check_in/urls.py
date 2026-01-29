9aeda9b8872ee2ce4618e11b4ddaec8d3cf1b140
Yuri Kim <njs03332@gmail.com>
2026-01-28 17:49:56 -0800

    feat: #828 add API to check latest visibility setting for check-in

--- adoorback/check_in/urls.py
+++ adoorback/check_in/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.CurrentCheckIn.as_view(), name='current-check-in'),
     path('<int:pk>/', views.CheckInDetail.as_view(), name='check-in-detail'),
     path('read/<int:pk>/', views.CheckInRead.as_view(), name='check-in-read'),
+    path('latest-visibility/', views.CurrentUserLatestCheckInVisibility.as_view(), name='current-user-latest-check-in-visibility'),
 ]
