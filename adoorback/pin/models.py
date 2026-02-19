76c83ab8b73452ce6c34af5b46474cc75c5b5fed
Yuri Kim <njs03332@gmail.com>
2026-02-18 17:57:17 -0800

    bugfix: #842 delete pin when post is deleted

--- adoorback/pin/models.py
+++ adoorback/pin/models.py
@@ -1,22 +1,29 @@
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from adoorback.models import AdoorTimestampedModel
 
+from safedelete.models import SafeDeleteModel
+from safedelete.models import SOFT_DELETE_CASCADE
+
 User = get_user_model()
 
-class Pin(AdoorTimestampedModel):
+class Pin(AdoorTimestampedModel, SafeDeleteModel):
     user = models.ForeignKey(User, related_name='pin_set', on_delete=models.CASCADE)
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
 
+    _safedelete_policy = SOFT_DELETE_CASCADE
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'content_type', 'object_id'],
+                condition=Q(deleted__isnull=True),
                 name='unique_pin'
             )
         ]
