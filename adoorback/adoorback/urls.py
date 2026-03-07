5b14b0b6e84c807466f04a75d5007e3a70ab0f5a
Yuri Kim <njs03332@gmail.com>
2026-03-07 15:03:56 -0800

    refactor: #857 remove pin feature

--- adoorback/adoorback/urls.py
+++ adoorback/adoorback/urls.py
@@ -45,7 +45,6 @@ urlpatterns = [
     path('api/ping/', include('ping.urls')),
 
     path('api/translate/', include('translate.urls')),
-    path('api/pins/', include('pin.urls')),
 ]
 
 if settings.DEBUG:
--- adoorback/note/models.py
+++ adoorback/note/models.py
@@ -59,8 +59,6 @@ class Note(AdoorModel, SafeDeleteModel):
     note_originated_notis = GenericRelation(Notification,
                                             content_type_field='origin_type',
                                             object_id_field='origin_id')
-    note_pins = GenericRelation('pin.Pin')
-
     _safedelete_policy = SOFT_DELETE_CASCADE
 
     def __str__(self):
