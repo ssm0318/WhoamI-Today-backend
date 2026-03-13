b5bf44f4ab7781c25812bef3e4b6ef31f96725e3
Yuri Kim <njs03332@gmail.com>
2026-03-13 16:36:57 -0700

    feat: #864 add API for has_posted_note_today

--- adoorback/account/tests.py
+++ adoorback/account/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from account.models import Connection
 from account.serializers import UserProfileSerializer
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, APIClient
 
 User = get_user_model()
 
@@ -24,3 +24,29 @@ class ProfileCountTests(TestCase):
         data = serializer.data
 
         self.assertEqual(data.get('friend_count'), 1)
+
+
+class CurrentUserNoteStatusTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test_user',
+            password='password',
+            email='test@example.com',
+            timezone='Asia/Seoul'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.url = '/api/user/me/note-status/'
+
+    def test_note_status_without_note(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.data['has_posted_note_today'])
+
+    def test_note_status_with_note_today(self):
+        from note.models import Note
+        Note.objects.create(author=self.user, content='test note today')
+        
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.data['has_posted_note_today'])
