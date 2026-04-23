68bb07cf019bd1dcfb7b10481cd8cef6ae3ac283
Gina Park <koyrkr@gmail.com>
2026-04-23 09:38:57 +0900

    Revert "feat(version): dispatch FCM silent push on current_ver change for ins… (#910)"
    This reverts commit 12c3da94f3fc40d902c3fc63855de49bad12b7eb.

--- adoorback/account/apps.py
+++ adoorback/account/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     name = 'account'
     default_auto_field = 'django.db.models.AutoField'
-
-    def ready(self):
-        from . import signals  # noqa: F401
